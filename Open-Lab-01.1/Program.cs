2342beca3e37468e802137cb58c72aa9487f6a87
samuel zaťovič <szatovic@gmail.com>
2019-10-11 15:08:22 +0200

    Hotový program

--- Open-Lab-01.1/Program.cs
+++ Open-Lab-01.1/Program.cs
@@ -6,9 +6,30 @@ namespace Open_Lab_01._1
     {
         static void Main(string[] args)
         {
-            int a = 5;
-            Console.WriteLine("Hello There!!");
-            Console.WriteLine(a);
+            Console.WriteLine("Please, insert a number between " + int.MinValue + " and " + int.MaxValue);
+            string line;
+            line = Console.ReadLine();
+
+            if (line == null || line == "") {
+                Console.WriteLine("Empty input");
+                return;
+            }
+            int number;
+            if (!int.TryParse(line, out number))
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+            if (number >= 10)
+            {
+                Console.WriteLine("This number is greater or equal to 10.");
+            }
+            else
+            {
+                Console.WriteLine("This number is less then 10.");
+            }
+           
+
         }
     }
 }
